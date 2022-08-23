ad597fff95ad442035656b1dfb7a3cdeb439c5b4
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-08-23 14:02:14 +0800

    Update Character.java

--- week2/without_constructor/Character.java
+++ week2/without_constructor/Character.java
@@ -1,28 +1,14 @@
-public class App {
-    public static void main(String[] args) {
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
 
-        Character knight = new Character();
-
-        knight.strength = 10;
-        knight.agility = 5;
-        knight.intelligence = 0;
-        knight.name = "Chen";
-        knight.sayMyName();
-
-        Character mage = new Character();
-
-        mage.strength = 0;
-        mage.agility = 5;
-        mage.intelligence = 10;
-        mage.name = "Akasha";
-        mage.sayMyName();
-
-        Character thief = new Character();
-
-        thief.strength = 5;
-        thief.agility = 10;
-        thief.intelligence = 0;
-        mage.name = "Gondar";
-        thief.sayMyName();
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+    
+    public void sayMyStrength() {
+        System.out.println("My strength is " + strength);
     }
 }
