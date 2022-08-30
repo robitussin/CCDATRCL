d5fb9dc921e328285fd52a94e511afbe7225d591
robitussin <elizerponiojr@gmail.com>
2022-08-30 18:22:28 +0800

    Updated comments

--- week_3/framework_implementation/App.java
+++ week_3/framework_implementation/App.java
@@ -3,39 +3,44 @@ import java.util.LinkedList;
 class Main {
     public static void main(String[] args) {
 
-        // create linkedlist
+        // Creates a new Linkedlist object
         LinkedList<String> Playlist = new LinkedList<>();
 
         // Real world examples: Train, music/video playlists
 
-        // Add elements to LinkedList
+        // Add new nodes to LinkedList
         Playlist.add("Pare ko");
         Playlist.add("Overdrive");
         Playlist.add("Alapaap");
+
+        // Display all nodes in the LinkedList
         System.out.println("Current playlist: " + Playlist);
 
-        // add() method with the index parameter
+        // Adds a new node at a certain position in the LinkedList
         Playlist.add(1, "With a Smile");
+
+        // Display all nodes in the LinkedList
         System.out.println("Updated playlist: " + Playlist);
 
-        // get the element from the linked list
+        // Retrieves the node at a certain position from the LinkedList
         String str = Playlist.get(1);
+
+        // Display node at position 1 in the LinkedList
         System.out.print("Element at index 1: " + str);
 
-        // change elements at index 3
+        // Change node data at index 3 in the LinkedList
         Playlist.set(2, "Magasin");
+
+        // Display node at position 1 in the LinkedList
         System.out.println("Updated playlist: " + Playlist);
 
-        // remove elements from index 1
+        // Remove node at index 1
         Playlist.remove(1);
 
+        // Display all nodes in the LinkedList
         System.out.println("Updated playlist: " + Playlist);
 
-        Playlist.addFirst("Monkey");
-
-        System.out.println("Updated playlist: " + Playlist);
-
-        // Iterate through playlist using for each
+        // Iterate through LinkedList
         for (String song : Playlist) {
             System.out.println(song);
         }
