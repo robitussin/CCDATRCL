4dd1327e1e74c8d05dd458a3dfd5cb568865c1b8
robitussin <elizerponiojr@gmail.com>
2022-09-10 16:04:57 +0800

    Added linkedlist implementation of queue

---
+++ week_5/manual_implementation/linkedlist_implementation_of_queue/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) {
+        Queue HotelReservation = new Queue();
+
+        // Adds elements to the queue
+        HotelReservation.add("Elizer");
+        HotelReservation.add("Lawrence");
+        HotelReservation.add("Neil");
+        HotelReservation.add("Mariel");
+        HotelReservation.add("Jane");
+
+        // Display all elements of the queue.
+        HotelReservation.printQueue();
+
+        // Removes the element at the front of the queue
+        HotelReservation.remove();
+
+        // Returns the number of elements in the queue.
+        System.out.println(HotelReservation.size());
+
+        // Shows the element at the front of the queue
+        System.out.println(HotelReservation.peek());
+
+        // Display all elements of the queue.
+        HotelReservation.printQueue();
+    }
+}
---
+++ week_5/manual_implementation/linkedlist_implementation_of_queue/Queue.java
@@ -0,0 +1,105 @@
+class Queue {
+
+    class Node {
+
+        // Node Properties
+        String data;
+        Node next;
+
+        // constructor to create a new linked list node
+        public Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Queue Properties
+    static Node front;
+    static Node rear;
+    static int nodeCount;
+
+    // Constructor
+    public Queue() {
+        front = null;
+        rear = null;
+        nodeCount = 0;
+    }
+
+    // Adds a new element to the queue
+    void add(String data) {
+
+        // Create a new Linked list node
+        Node temp = new Node(data);
+
+        System.out.println("Inserting " + data);
+
+        // Increase node count by 1
+        nodeCount += 1;
+
+        // If queue is empty, then new node is front and rear both
+        if (rear == null) {
+            front = temp;
+            rear = temp;
+            return;
+        }
+
+        // Add the new node at the end of queue and change rear
+        rear.next = temp;
+        rear = temp;
+    }
+
+    // Removes the element at the front of the queue.
+    void remove() {
+        // If queue is empty, return NULL.
+        if (front == null) {
+            System.out.println("The queue is empty!");
+            return;
+        }
+
+        System.out.println("Removing " + front.data);
+
+        // Store previous front and move front one node ahead
+        front = front.next;
+
+        // If front becomes NULL, then change rear also as NULL
+        if (front == null)
+            rear = null;
+
+        // Decrease node count by 1
+        nodeCount -= 1;
+    }
+
+    // Returns the number of elements in the queue.
+    public int size() {
+        return nodeCount;
+    }
+
+    // Returns the element at the front of the queue.
+    public String peek() {
+        return front.data;
+    }
+
+    // Displays all elements in the queue
+    public void printQueue() {
+
+        Node current = Queue.front;
+
+        int count = Queue.nodeCount;
+
+        System.out.print("[");
+        while (count > 0) {
+
+            System.out.print(current.data);
+
+            if (count > 1) {
+                System.out.print(", ");
+            }
+
+            current = current.next;
+            count--;
+        }
+
+        System.out.print("]");
+        System.out.println("");
+    }
+}
