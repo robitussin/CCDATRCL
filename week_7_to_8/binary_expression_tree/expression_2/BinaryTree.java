42dc30ccd532e27ec40e296a6f2b1060161cc6ca
robitussin <elizerponiojr@gmail.com>
2022-09-26 22:39:28 +0800

    added expression 1 and 2

---
+++ week_7_to_8/binary_expression_tree/expression_2/BinaryTree.java
@@ -0,0 +1,45 @@
+// Binary Tree in Java
+public class BinaryTree {
+
+    public static void main(String[] args) {
+
+        // Create root node A
+        Node A = new Node('+');
+        // Create node B
+        Node B = new Node('*');
+        // Create node C
+        Node C = new Node('/');
+        // Create node D
+        Node D = new Node('8');
+        // Create node E
+        Node E = new Node('2');
+        // Create node F
+        Node F = new Node('6');
+        // Create node G
+        Node G = new Node('2');
+
+        // Set left and right child of root node A
+        A.left = B;
+        A.right = C;
+
+        // Set left and right child of node B
+        B.left = D;
+        B.right = E;
+
+        // Set left and right child of node B
+        C.left = F;
+        C.right = G;
+
+        System.out.print("\nIn order Traversal: ");
+        traverseInOrder(A);
+    }
+
+    // Traverse Inorder Method
+    static void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseInOrder(node.right);
+        }
+    }
+}
