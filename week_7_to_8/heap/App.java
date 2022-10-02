9b7f387ec9c8ff860451815e893002bae8980ffc
robitussin <elizerponiojr@gmail.com>
2022-10-02 22:30:57 +0800

    Added heap

---
+++ week_7_to_8/heap/App.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        int size = array.size();
+
+        Heap h = new Heap();
+        h.insert(array, 3);
+        h.insert(array, 4);
+        h.insert(array, 9);
+        h.insert(array, 5);
+        h.insert(array, 2);
+
+        System.out.println("Max-Heap array: ");
+        h.printArray(array, size);
+
+        h.deleteNode(array, 4);
+        System.out.println("After deleting an element: ");
+        h.printArray(array, size);
+    }
+}
