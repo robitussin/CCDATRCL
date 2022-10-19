197f287ebb38a8b33d63d97d22c52308e018450c
robitussin <elizerponiojr@gmail.com>
2022-10-19 15:56:15 +0800

    Added week 10

---
+++ week_10/graphs/graph/Graph.java
@@ -0,0 +1,60 @@
+public class Graph {
+
+    // inner class
+    // to keep track of edges
+    class Edge {
+        String src, dest;
+    }
+
+    // number of vertices and edges
+    int vertices, edges;
+
+    // array to store all edges
+    Edge[] edge;
+
+    Graph(int vertices, int edges) {
+        this.vertices = vertices;
+        this.edges = edges;
+
+        // initialize the edge array
+        edge = new Edge[edges];
+        for (int i = 0; i < edges; i++) {
+
+            // each element of the edge array
+            // is an object of Edge type
+            edge[i] = new Edge();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // create an object of Graph class
+        int noVertices = 4;
+        int noEdges = 4;
+        Graph myGraph = new Graph(noVertices, noEdges);
+
+        // create graph
+
+        // Edge 1
+        myGraph.edge[0].src = "Winnah Jane"; // edge mark---jay
+        myGraph.edge[0].dest = "Nica";
+
+        // Edge 2
+        myGraph.edge[1].src = "Winnah Jane"; // edge mark---michelle
+        myGraph.edge[1].dest = "Jonnah";
+
+        // Edge 3
+        myGraph.edge[2].src = "Winnah Jane"; // edge michelle---jay
+        myGraph.edge[2].dest = "Ronn";
+
+        // Edge 4
+        myGraph.edge[3].src = "Nica"; // edge jay---anne
+        myGraph.edge[3].dest = "Jonnah";
+
+        // print graph
+        for (int i = 0; i < noEdges; i++) {
+            System.out.println(myGraph.edge[i].src + " - " + myGraph.edge[i].dest);
+        }
+
+    }
+}
